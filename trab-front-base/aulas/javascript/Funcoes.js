89aeb9df9203d07b1211c3b452d744a0df183b4e
calopsita67 <rogersambulski@gmail.com>
2025-11-05 19:20:20 -0300

    Add basic arithmetic functions in Funcoes.js

---
+++ trab-front-base/aulas/javascript/Funcoes.js
@@ -0,0 +1,22 @@
+//adição
+function adicao(n1, n2) {
+  return n1 + n2
+}
+const resultadoAdicao = adicao(10, 20 ) + 30
+console.log(`adição: ${resultadoAdicao}`)
+// subtraçao
+function sub(n1, n2) {
+  return n1 - n2
+}
+const resultadoSub = sub(60, 20) - 10
+console.log(`Subtração: ${resultadoSub}`)
+// divisao
+function div(n1, n2) {
+  return n1 / n2
+}
+const resultadoDiv = div(40, 2) / 2
+console.log(`Divisão: ${resultadoDiv}`)
+// Multiplicação
+function vezes(n1, n2) {
+  return n1 * n2
+}
