bf4dd81edd2e86e3e675862c0ba92393434a2a76
calopsita67 <rogersambulski@gmail.com>
2025-11-12 20:52:17 -0300

    Update produtos.js

--- trab-front-base/aulas/cadastro-produtos/produtos.js
+++ trab-front-base/aulas/cadastro-produtos/produtos.js
@@ -1,24 +1,32 @@
 const formulario = document.querySelector('#form-user');
 const titulo = document.querySelector('#nome');
-const desc = document.querySelector('#descricao'); // troque o id no HTML também
+const desc = document.querySelector('#descricao');
 const urlimage = document.querySelector('#imagem');
 const btnremover = document.querySelector('#remover');
 const containerCards = document.querySelector('#container-cards');
 
-const ItemProdutos = [
-  // { nome: "Prod1", descricao: "", url: "https://dfaffs" },
-  // { nome: "Prod2", descricao: "", url: "https://dfaffs" },
-];
+const ItemProdutos = [];
 
-// Escutador de evento (submit do formulário)
 formulario.addEventListener('submit', function(event) {
   event.preventDefault();
   alert('Funfou!');
+  const titulo = document.querySelector('#nome');
+    const desc = document.querySelector('#descricao');
+    const urlimage = document.querySelector('#imagem');
 
-  // Aqui você pode pegar os valores dos campos, por exemplo:
-  console.log(titulo.value, desc.value, urlimage.value);
+  
+  const novoProduto = {
+    nome: titulo.value,
+    descricao: desc.value,
+    url: urlimage.value
+  };
 
-  // Depois pode resetar o formulário se quiser:
-  // formulario.reset();
+  
+  ItemProdutos.push(novoProduto);
+
+  console.log(ItemProdutos); 
+ 
+  formulario.reset();
+
+  
 });
-
